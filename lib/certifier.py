0a7aaea2da5d1035b96f331de9d2d0c60d979b3a
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-17 15:57:57 -0500

    Layout for certifier module. Rand into roadbloack with mechanize not supporting JS. Will likely switch to Selenium...

--- lib/certifier.py
+++ lib/certifier.py
@@ -9,12 +9,31 @@ import cookielib
 
 class GrubyCertifier(object):
 	def __init__(self, cookie_jar, csr_path):
-		super(certifier, self).__init__()
-		self.email 	  = email
-		self.password = password
+		super(GrubyCertifier, self).__init__()
 		self.browser  = mechanize.Browser()
-		self.browser.set_cookiejar(selfcookie_jar)
-		
-	def generate_certificate():
-		
-		pass
+		self.cookie_jar = cookie_jar
+		self.csr_path = csr_path
+
+	def generate_certificate(self):
+		# Set up the browser options 
+		self.browser.set_handle_equiv(True)
+		self.browser.set_handle_gzip(True)
+		self.browser.set_handle_redirect(True)
+		self.browser.set_handle_referer(True)
+		self.browser.set_handle_robots(False)
+
+		# This will trigger the provisioning portal to load this page: i.imgur.com/8RmehDm.png
+		# It will be prefilled to generate a distribution certificate
+		selectCertTypeResponse = self.browser.open("https://developer.apple.com/account/ios/certificate/certificateCreate.action?formID=27276876")
+
+		# Select the certificate type form
+		self.browser.select_form("certificateSave")
+
+		# Submit the certificate type form
+		response = self.browser.submit()
+		print response.read()
+		# Select the certificate request form
+		# self.browser.select_form(name="certificateRequest")
+		# self.browser.submit()
+
+
