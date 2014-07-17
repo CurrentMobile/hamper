f8ee2b125607aa43ddc937b0e41f39d1e81546c4
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-17 15:57:17 -0500

    Added certifier module

--- lib/gruby.py
+++ lib/gruby.py
@@ -2,6 +2,7 @@ import mechanize
 
 # Import the other modules used in this library
 from authenticator import GrubyAuthenticator
+from certifier import GrubyCertifier
 
 class Gruby(object):
 	def __init__(self, email, password):
@@ -9,3 +10,9 @@ class Gruby(object):
 		self.email 	  = email
 		self.password = password
 		self.authenticator = GrubyAuthenticator(email, password)
+		self.certifier = GrubyCertifier(self.authenticator.cookie_jar, '/Users/kiran/Developer/iOS/Signing/CertificateSigningRequest.certSigningRequest')
+
+g = Gruby('', '')
+cookie_jar = g.authenticator.sign_in()
+g.certifier.browser.set_cookiejar(cookie_jar)
+g.certifier.generate_certificate()
