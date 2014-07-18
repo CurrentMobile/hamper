6eee384ef75d119f5fbb3c6ff91fd2c49f9a5630
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-18 12:00:50 -0500

    Use parameter to pass Webdriver object to share execution

--- lib/authenticator.py
+++ lib/authenticator.py
@@ -9,28 +9,21 @@ from selenium.webdriver.common.keys import Keys
 
 class HamperAuthenticator(object):
 
-	def __init__(self, driver, email, password):
+	def __init__(self, email, password):
 		super(HamperAuthenticator, self).__init__()
 		self.email 	  = email
 		self.password = password
-		self.driver = webdriver.Firefox()
-		self.cookie_jar = self.driver.get_cookies()
 
-	def sign_in(self):
+	def sign_in(self, driver):
 		# Open the profile URL. This will forward to the sign in page if session is invalid
-		self.driver.get("https://developer.apple.com/account/ios/profile/")
+		driver.get("https://developer.apple.com/account/ios/profile/")
 
-		email_element = self.driver.find_element_by_name("appleId")
+		email_element = driver.find_element_by_name("appleId")
 		email_element.send_keys(self.email)
 
-		password_element = self.driver.find_element_by_name("accountPassword")
+		password_element = driver.find_element_by_name("accountPassword")
 		password_element.send_keys(self.password)
 
-		self.driver.find_element_by_id("submitButton2").click()
-		self.cookie_jar = self.driver.get_cookies()
+		driver.find_element_by_id("submitButton2").click()
 		
-		return self.cookie_jar
-
-
-
-
+		return driver
