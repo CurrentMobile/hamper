e80e63b6a83e8c99aa597b1cce5eff002352b1f6
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-21 15:02:37 -0500

    removed WebDriver property (now that modules are using the HamperDriver singleton object)

--- lib/hamper.py
+++ lib/hamper.py
@@ -1,23 +1,19 @@
-import mechanize
-
 # Import the other modules used in this library
 from authenticator import HamperAuthenticator
 from certifier import HamperCertifier
 from error import HamperError
 
-from selenium import webdriver
-import inspect
+from helpers.driver import HamperDriver
 
 class Hamper(object):
 	def __init__(self, email, password):
 		super(Hamper, self).__init__()
 		self.email 	  = email
 		self.password = password
-		self.driver	  = webdriver.Chrome()
 
 		self.authenticator = HamperAuthenticator(email, password)
 		self.certifier = HamperCertifier()
 
 h = Hamper(email='', password='')
-h.authenticator.sign_in(h.driver)
-print h.certifier.generate_distribution_certificate(h.driver, "/Users/kiran/Developer/iOS/Signing/CertificateSigningRequest.certSigningRequest")
+h.authenticator.sign_in()
+print h.certifier.generate_distribution_certificate("/Users/kiran/Developer/iOS/Signing/CertificateSigningRequest.certSigningRequest")
