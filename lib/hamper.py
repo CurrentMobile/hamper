177a6b170213e925532aef4d6bfeb5614fa267b2
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-21 09:05:26 -0500

    Switched driver to use Chrome over Firefox

--- lib/hamper.py
+++ lib/hamper.py
@@ -3,15 +3,17 @@ import mechanize
 # Import the other modules used in this library
 from authenticator import HamperAuthenticator
 from certifier import HamperCertifier
+from error import HamperError
 
 from selenium import webdriver
+import inspect
 
 class Hamper(object):
 	def __init__(self, email, password):
 		super(Hamper, self).__init__()
 		self.email 	  = email
 		self.password = password
-		self.driver	  = webdriver.Firefox()
+		self.driver	  = webdriver.Chrome()
 
 		self.authenticator = HamperAuthenticator(email, password)
 		self.certifier = HamperCertifier('/Users/kiran/Developer/iOS/Signing/CertificateSigningRequest.certSigningRequest')
