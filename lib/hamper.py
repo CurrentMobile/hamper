bd0c7f6fc2e0e229853893c1f829e6ebb755a1ca
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-23 15:31:17 -0500

    Added CLI for certificate generation. Cache the current email address and then store the email/pass combination in the user's keychain

--- lib/hamper.py
+++ lib/hamper.py
@@ -6,7 +6,7 @@ Hamper
 
 Usage:
   hamper.py auth login --email=<email> --password=<password>
-  hamper.py cert create --type=<cert_type> --csr_path=<csr_path> [--app_id=<app_id>]
+  hamper.py cert create (development | development_push | distribution | distribution_push) --csr_path=<csr_path> [--bundle_id=<bundle_id>]
   hamper.py identifier create --app_name=<app_name> --bundle_id=<bundle_id> [--enabled_services=<app_service>...]
   hamper.py profile create --name=<profile_name> --bundle_id=<bundle_id> --type=<profile_type> [(--exp_day=<exp_day> --exp_month=<exp_month> --exp_year=<exp_year>)]
 
@@ -26,6 +26,8 @@ from helpers.error import HamperError
 from docopt import docopt
 from termcolor import colored
 
+import keyring
+
 class Hamper(object):
 	def __init__(self):
 		super(Hamper, self).__init__()
@@ -37,19 +39,55 @@ class Hamper(object):
 
 h = Hamper()
 
+def save_login_details(email, password):
+	keyring.set_password("hamper", email, password)
+	open('session', 'w').close()
+	open("session", "w").write(email)
+
+def cached_login_details():
+	with open('session', 'r') as content_file:
+		content = content_file.read()
+		return content, keyring.get_password("hamper", content)
+
 def handle_auth_action(arguments):
 	try:
 		h.authenticator.sign_in(email=arguments['--email'], password=arguments['--password'])
+		save_login_details(arguments['--email'], arguments['--password'])
+
 		print colored("User successfully authenticated.", "green")
 	except Exception, e:
 		if len(e.args) > 0 and hasattr(e.args[0], 'message'):
-			print colored(e.args[0].message, "red")
+			print colored("ERROR: " + e.args[0].message, "red")
 		else:
 			print colored(e, "red")
 		
 
 def handle_cert_action(arguments):
-	pass
+	try:
+		cached_email, cached_password = cached_login_details()
+		h.authenticator.sign_in(email=cached_email, password=cached_password)
+
+		print colored("User successfully authenticated.", "green")
+
+		if arguments['development']:
+			h.certifier.generate_development_certificate(arguments['--csr_path'])
+
+		elif arguments['development_push']:
+			h.certifier.generate_development_push_certificate(arguments['--bundle_id'], arguments['--csr_path'])
+
+		elif arguments['distribution']:
+			h.certifier.generate_distribution_certificate(arguments['--csr_path'])
+
+		elif arguments['distribution_push']:
+			h.certifier.generate_distribution_push_certificate(arguments['--bundle_id'], arguments['--csr_path'])
+
+		print colored("Certificate successfully generated.", "green")
+	except Exception, e:
+		if len(e.args) > 0 and hasattr(e.args[0], 'message'):
+			print colored("ERROR: " + e.args[0].message, "red")
+		else:
+			print colored(e, "red")
+
 	
 def handle_identifier_action(arguments):
 	pass
