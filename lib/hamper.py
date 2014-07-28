eafccd0b9a87898560c0cd5045b4612564a9a170
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-28 08:44:56 -0500

    Added comments, moved try-catch to parent parse_arguments() method

--- lib/hamper.py
+++ lib/hamper.py
@@ -26,8 +26,10 @@ from helpers.error import HamperError
 from docopt import docopt
 from termcolor import colored
 
+# Used for access the computer's keychain (for storing login credentials)
 import keyring
 
+# Create the class for holding all the Hamper instances
 class Hamper(object):
 	def __init__(self):
 		super(Hamper, self).__init__()
@@ -37,27 +39,48 @@ class Hamper(object):
 		self.identifier    = HamperIdentifier()
 		self.provisioner   = HamperProvisioner()
 
+# Instantiate this thing
 h = Hamper()
 
+# Helper method to save the login details
 def save_login_details(email, password):
+
+	# Save the username and password to the keychain
 	keyring.set_password("hamper", email, password)
+
+	# Store the current email address being used to a text file.
+	# This allows us to maintain a persistant 'session'.
+	# When the user tries to execute a Hamper action (create a certificate, for example), Hamper will find the 
+	# cached email address stored in the 'session' file, then fetch the corresponding password from the keychain.
+	# Then, Hamper will run the HamperAuthenticator method with these credentials to sign the user in and create the WebDriver session.
 	open('session', 'w').close()
 	open("session", "w").write(email)
 
+# Helper method to fetch the cached login details
 def cached_login_details():
+
+	# Get the currently-auth'd email address
 	with open('session', 'r') as content_file:
 		content = content_file.read()
+
+		# Grab the login credentials from the keychain
 		return content, keyring.get_password("hamper", content)
 
+# ----
+# Methods to handle the CLI actions
+# ----
+
+
+#
+# Called when the user tries 'hamper auth ...' 
+# Handles the login actions
+#
 def handle_auth_action(arguments):
-	try:
-		h.authenticator.sign_in(email=arguments['--email'], password=arguments['--password'])
-		save_login_details(arguments['--email'], arguments['--password'])
-	except Exception, e:
-		if len(e.args) > 0 and hasattr(e.args[0], 'message'):
-			print colored("ERROR: " + e.args[0].message, "red")
-		else:
-			print colored(e, "red")
+	# Sign in with the provided credentials
+	h.authenticator.sign_in(email=arguments['--email'], password=arguments['--password'])
+
+	# If the sign in was successful, save the credentials to the keychain and session file
+	save_login_details(arguments['--email'], arguments['--password'])
 		
 def handle_cert_action(arguments):
 	try:
@@ -143,14 +166,20 @@ def handle_profile_action(arguments):
 			print colored(e, "red")
 
 def parse_arguments(arguments):
-	if arguments['auth'] == True:
-		handle_auth_action(arguments)	
-	elif arguments['cert'] == True:
-		handle_cert_action(arguments)
-	elif arguments['identifier'] == True:
-		handle_identifier_action(arguments)
-	elif arguments['profile'] == True:
-		handle_profile_action(arguments)
+	try:
+		if arguments['auth'] == True:
+			handle_auth_action(arguments)	
+		elif arguments['cert'] == True:
+			handle_cert_action(arguments)
+		elif arguments['identifier'] == True:
+			handle_identifier_action(arguments)
+		elif arguments['profile'] == True:
+			handle_profile_action(arguments)
+	except Exception, e:
+		if len(e.args) > 0 and hasattr(e.args[0], 'message'):
+			print colored("ERROR: " + e.args[0].message, "red")
+		else:
+			print colored(e, "red")
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Hamper 0.1')
