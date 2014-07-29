bfdb8921416d7a586fd47381b8b5d003e7088e33
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-29 17:38:28 -0500

    Updated pip setup files and changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HamperCLI',
-    version='0.1.1',
+    version='0.1.5',
     author='Kiran Panesar',
     author_email='kiransinghpanesar@googlemail.com',
     packages=['hamper', 'hamper/helpers'],
