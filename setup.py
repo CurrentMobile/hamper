8556ae939c54384c4408130b9d0755dc399fddb5
Kiran Panesar <kiran@mobilexlabs.com>
2014-08-26 13:40:42 -0500

    Bumped version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HamperCLI',
-    version='1.0.1',
+    version='1.0.2',
     author='Kiran Panesar',
     author_email='kiransinghpanesar@googlemail.com',
     packages=['hamper', 'hamper/helpers'],
